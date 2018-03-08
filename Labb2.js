4d2a634eae1d430e8bb9504093ded597e6de20c4
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-03-08 14:53:48 +0100

    added API, ajax

--- Labb2.js
+++ Labb2.js
@@ -12,32 +12,81 @@ window.addEventListener('load', function(){
     let editButton = document.getElementById('editButton');
     let list = document.getElementById('list');
 
-    //old code
-/*function insert () {
-    titles.push(titleInput.value);
-    authors.push(authorInput.value);
+    //API
+    let viewAjaxBtn = document.getElementById('viewAjaxBtn');
+    let responseLabel = document.getElementById('response');
+    let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?';
+    let requestKey = 'reguestKey';
+    let key = 'j3pod';
+    const viewQuery = `op=select&key=${key}`;
+    let addQuery = `op=insert&title=${titleInput.value}&author=${titleInput.value}&key=${key}`;
 
-    // Clean and print output
-    titleInput.value = '';
-    authorInput.value = '';
-
-        listBox.innerHTML = '';
-        listBox.innerHTML += titles + ', By ' + authors + '</br>';
-}*/
     function Clear (){
         titleInput.value = '';
         authorInput.value = '';
     }
 
-    addButton.addEventListener('click', function(event) { 
-        if(!titleInput){
-            const li = document.createElement('li');
-            li.innerText = titleInput.value + ', by ' + authorInput.value;
-            li.innerHTML = `<div id="box">${titleInput.value}, by ${authorInput.value} <button id="editButton">edit</button> <button id="deleteOneButton">remove</button></div>`;
-            list.appendChild(li);
+    function addToList(){
+        const li = document.createElement('li');
+        li.innerText = titleInput.value + ', by ' + authorInput.value;
+        li.innerHTML = `<div id="box">${titleInput.value}, by ${authorInput.value} <button id="editButton">edit</button> <button id="deleteOneButton">remove</button></div>`;
+        list.appendChild(li);
 
-            Clear();
-        }
+        Clear();
+    }
+
+    function addAjax(){
+        fetch(url+addQuery)
+        .then(response => { 
+          return response.json(); //om det är json data retuneras ett promise.
+        })
+        .then(json => {
+          console.log('We got json object', json);
+          addToList();
+        })
+        .catch(obj => {
+          responseLabel.innerText = 'ERROR' + obj;
+          if(obj.status == 'success'){
+            console.log(obj);
+            console.log(obj.data);
+          }
+          else (obj.status == 'error')
+              return addAjax();
+          //anropa funktionen rekursivt här?? 
+        })
+        console.log('request sent');
+      }
+
+      function viewAjax(){
+        fetch(url+viewQuery)
+        .then(response => { 
+          return response.json(); //om det är json data retuneras ett promise.
+        })
+        .then(json => {
+          console.log('Viewing data: ', json);
+          responseLabel.innerText = json; //ta bort detta sedan
+          viewAjaxBtn.disabled = false;
+        })
+        .catch(obj => {
+          responseLabel.innerText = 'ERROR' + obj;
+          viewAjaxBtn.disabled = false;
+
+          if(obj.status == 'success'){
+            console.log(obj);
+            console.log(obj.data);
+          }
+          else
+              return viewAjax();
+          //anropa funktionen rekursivt här?? 
+
+        })
+        console.log('request sent');
+      }
+
+     //lägg in ifsats med counter och rekursivt om det blir error så anropas den igen tills det blir success
+
+    addButton.addEventListener('click', function(event) { 
+        addAjax();
     });
 
     deleteButton.addEventListener('click', function(event) {
@@ -46,7 +95,12 @@ window.addEventListener('load', function(){
 
     });
 
-    deleteOneButton.addEventListener('click', function(event) {
+    viewAjaxBtn.addEventListener('click', function(event){
+        viewAjax();
+    });
+
+});
+   /* deleteOneButton.addEventListener('click', function(event) {
         
         //code to remove one element
 
@@ -56,5 +110,5 @@ window.addEventListener('load', function(){
         
         //code to edit one element
 
-    });
-});
+    });*/
+
