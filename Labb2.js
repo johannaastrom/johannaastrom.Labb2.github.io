4fbe7c94489417df86f5ace0f8fa055dd495a6cd
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-03-11 13:54:40 +0100

    fixed ajax bug and added counter.

--- Labb2.js
+++ Labb2.js
@@ -16,74 +16,69 @@ window.addEventListener('load', function(){
     let viewAjaxBtn = document.getElementById('viewAjaxBtn');
     let responseLabel = document.getElementById('response');
     let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?';
-    let requestKey = 'reguestKey';
-    let key = 'j3pod';
+    let requestKey = 'requestKey';
+    let key = '1pN6M'; //tidigare: j3pod
     const viewQuery = `op=select&key=${key}`;
     let addQuery = `op=insert&title=${titleInput.value}&author=${titleInput.value}&key=${key}`;
+    let counter = 0;
 
-    function Clear (){
+    function ClearTextboxes (){
         titleInput.value = '';
         authorInput.value = '';
     }
 
-    function addToList(){
+    function addItemToList(){
         const li = document.createElement('li');
         li.innerText = titleInput.value + ', by ' + authorInput.value;
         li.innerHTML = `<div id="box">${titleInput.value}, by ${authorInput.value} <button id="editButton">edit</button> <button id="deleteOneButton">remove</button></div>`;
         list.appendChild(li);
 
-        Clear();
+        ClearTextboxes();
     }
 
-    function addAjax(){
+    function addAjax(){ //fixa denna function, varför skickas bara tomma objekt?? + fixa if sats om error eller success
         fetch(url+addQuery)
         .then(response => { 
+          console.log('added: ' + authorInput.value + ', ' + titleInput.value); //vad är detta
           return response.json(); //om det är json data retuneras ett promise.
         })
         .then(json => {
-          console.log('We got json object', json);
-          addToList();
+          console.log('Added new item: ', json);
+          addItemToList();
         })
         .catch(obj => {
           responseLabel.innerText = 'ERROR' + obj;
-          if(obj.status == 'success'){
-            console.log(obj);
-            console.log(obj.data);
-          }
-          else (obj.status == 'error')
-              return addAjax();
-          //anropa funktionen rekursivt här?? 
         })
         console.log('request sent');
       }
 
       function viewAjax(){
+
         fetch(url+viewQuery)
         .then(response => { 
-          return response.json(); //om det är json data retuneras ett promise.
+          return response.json(); //om det är json/text data retuneras ett promise.
         })
-        .then(json => {
-          console.log('Viewing data: ', json);
-          responseLabel.innerText = json; //ta bort detta sedan
-          viewAjaxBtn.disabled = false;
-        })
-        .catch(obj => {
-          responseLabel.innerText = 'ERROR' + obj;
-          viewAjaxBtn.disabled = false;
-
-          if(obj.status == 'success'){
-            console.log(obj);
-            console.log(obj.data);
-          }
-          else
-              return viewAjax();
-          //anropa funktionen rekursivt här?? 
-
-        })
-        console.log('request sent');
+        .then(obj =>{
+            if(counter === 1){
+              responseLabel.innerText = 'request denied, try again'
+              console.log(obj.data);
+              counter = 0;
+            }
+            else if(obj.status === 'success'){
+                console.log(obj);
+                console.log(counter);
+                console.log(obj.data);
+        
+                responseLabel.innerHTML = obj.data.map( book => `${book.title} by ${book.author} <br></br>` )
+              }
+              else{
+                counter++;
+                console.log(counter);
+                 viewAjax();
+               }
+            })
+        console.log('request sent'); //ha kvar detta eller inte?
       }
-
-     //lägg in ifsats med counter och rekursivt om det blir error så anropas den igen tills det blir success
 
     addButton.addEventListener('click', function(event) { 
         addAjax();
